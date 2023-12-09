e671cf8aeccefd1c4600183bc58f5d3acdf45d72
LyubovGarashchenko <145012335+LyubovGarashchenko@users.noreply.github.com>
2023-12-09 21:25:57 +0300

    Update3 Radio.java

--- src/main/java/ru/netology/statistic/Radio.java
+++ src/main/java/ru/netology/statistic/Radio.java
@@ -62,7 +62,7 @@ public class Radio {
     }
 
     public int hashCode() {
-        int PRIME = true;
+        int prime = true;
         int result = 1;
         result = result * 59 + this.getCurrentRadioStation();
         result = result * 59 + this.getCurrentVolume();
