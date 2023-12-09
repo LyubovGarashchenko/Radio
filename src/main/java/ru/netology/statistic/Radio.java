911ff2447a6f188c33caf9819ea6b0e84c17596f
LyubovGarashchenko <145012335+LyubovGarashchenko@users.noreply.github.com>
2023-12-09 21:35:27 +0300

    Update4 Radio.java

--- src/main/java/ru/netology/statistic/Radio.java
+++ src/main/java/ru/netology/statistic/Radio.java
@@ -37,41 +37,4 @@ public class Radio {
     public void setMaxCurrentStation(int maxCurrentStation) {
         this.maxCurrentStation = maxCurrentStation;
     }
-
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        } else if (!(o instanceof Radio)) {
-            return false;
-        } else {
-            Radio other = (Radio)o;
-            if (!other.canEqual(this)) {
-                return false;
-            } else if (this.getCurrentRadioStation() != other.getCurrentRadioStation()) {
-                return false;
-            } else if (this.getCurrentVolume() != other.getCurrentVolume()) {
-                return false;
-            } else {
-                return this.getMaxCurrentStation() == other.getMaxCurrentStation();
-            }
-        }
-    }
-
-    protected boolean canEqual(Object other) {
-        return other instanceof Radio;
-    }
-
-    public int hashCode() {
-        int prime = true;
-        int result = 1;
-        result = result * 59 + this.getCurrentRadioStation();
-        result = result * 59 + this.getCurrentVolume();
-        result = result * 59 + this.getMaxCurrentStation();
-        return result;
-    }
-
-    public String toString() {
-        int var10000 = this.getCurrentRadioStation();
-        return "Radio(currentRadioStation=" + var10000 + ", currentVolume=" + this.getCurrentVolume() + ", maxCurrentStation=" + this.getMaxCurrentStation() + ")";
-    }
 }
